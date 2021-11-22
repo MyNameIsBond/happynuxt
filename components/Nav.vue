6c975522ab0afd8f19da1e89830d726ef9e3e6c9
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2021-11-22 23:45:32 +0000

    first commit

---
+++ components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="w-full p-4 border-b">
+    <Logo class="h-9" />
+  </nav>
+</template>
+
+<script>
+import Logo from "../assets/logo.svg?inline";
+export default {
+  components: { Logo },
+};
+</script>
+
+<style></style>
