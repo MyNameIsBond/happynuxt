f9fa4a886242651a3ef4f47cd2e0ffe27dc5f27e
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2021-12-09 20:30:10 +0000

    Head added

--- components/MdArticle.vue
+++ components/MdArticle.vue
@@ -18,6 +18,36 @@
 <script>
 export default {
   props: ["page"],
+  head() {
+    return {
+      title: this.page.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.page.description,
+        },
+        // Open Graph
+        { hid: "og:title", property: "og:title", content: this.page.title },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content: this.page.description,
+        },
+        // Twitter Card
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content: this.page.title,
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content: this.page.description,
+        },
+      ],
+    };
+  },
 };
 </script>
 
