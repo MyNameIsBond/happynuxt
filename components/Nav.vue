d950995744bbc61138434648c8402a64952310ff
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-03-07 04:50:53 +0000

    test

--- components/Nav.vue
+++ components/Nav.vue
@@ -28,11 +28,6 @@ export default {
 </script>
 <style scoped>
 .bbl {
-  --tw-backdrop-blur: blur(24px);
-  backdrop-filter: var(--tw-backdrop-blur) var(--tw-backdrop-brightness)
-    var(--tw-backdrop-contrast) var(--tw-backdrop-grayscale)
-    var(--tw-backdrop-hue-rotate) var(--tw-backdrop-invert)
-    var(--tw-backdrop-opacity) var(--tw-backdrop-saturate)
-    var(--tw-backdrop-sepia);
+  backdrop-filter: blur(24px);
 }
 </style>
