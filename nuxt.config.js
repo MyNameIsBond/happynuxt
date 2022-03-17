0b4ed22e5bd8fa35a924e802a6096a8ccc676cd0
MyNameIsbond <hajdinim@uni.coventry.ac.uk>
2022-03-17 17:11:42 +0000

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components:  https://go.nuxtjs.dev/config-components
   target: "static",
   components: true,
 
